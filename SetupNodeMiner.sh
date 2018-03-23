4b14f79d092b7042e636b287bfe24007081b0854
Tobias Faber <tobias.faber@uni-saarland.de>
2018-03-23 22:21:30 +1300

    First full setup script.

---
+++ SetupNodeMiner.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Easy Setup Script for NodeJS Nimiq Miner
+
+#Get Settings
+echo 'Please enter your domain:'
+read nimiqDomain
+echo 'Please enter the number of Miningthreads'
+read nimiqThreads
+echo 'Enter the name of the script to be generated(e.g. mine.sh):'
+read nimiqScript
+echo 'Remove Apache if installed?(Type:YES)'
+read nimiqApache
+
+
+#Make sure system is updated
+sudo apt-get update
+sudo apt-get upgrade
+
+#Install requirements
+sudo apt-get install -y curl git build-essential
+
+#Setup NodeJS
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#Add Gulp globally
+sudo npm install -g gulp
+
+#Get the nimiq project
+git clone https://github.com/nimiq-network/core
+
+#Build nimiq project
+cd core
+sudo npm install
+sudo npm run build
+
+#setup Letsencrypt
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install -y certbot
+
+#remove apache2 if installed
+if [ "$nimiqApache" = "YES" ]; then
+    sudo apt-get remove apache2
+fi
+
+
+#Get SSL Cert
+sudo certbot certonly --standalone --preferred-challenges http -d nodeone.nimiqpool.com
+
+#Generate Mining Runscript
+cd ..
+touch $nimiqScript
+chmod +x $nimiqScript
+echo "cd core/clients/nodejs/" >> $nimiqScript 
+echo "node index.js --host=$nimiqDomain --port=5566 --cert=/etc/letsencrypt/live/$nimiqDomain/cert.pem --key=/etc/letsencrypt/live/$nimiqDomain/privkey.pem -miner=$nimiqThreads --statistics=1" >> $nimiqScript
