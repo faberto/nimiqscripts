7a80267674524df6acd1ff8d845f734f0edcb369
Faberto <github.faberto@itfaber.com>
2018-03-24 20:22:18 +1300

    Conflict removals,silent and wallet init using address

--- SetupNodeMiner.sh
+++ SetupNodeMiner.sh
@@ -2,15 +2,39 @@
 # Easy Setup Script for NodeJS Nimiq Miner
 
 #Get Settings
-echo 'Please enter your domain:'
+echo 'Please enter your domain: '
 read nimiqDomain
-echo 'Please enter the number of Miningthreads'
+echo 'Please enter the number of Miningthreads: '
 read nimiqThreads
-echo 'Enter the name of the script to be generated(e.g. mine.sh):'
+echo 'Enter the name of the script to be generated(e.g. mine.sh): '
 read nimiqScript
-echo 'Remove Apache if installed?(Type:YES)'
-read nimiqApache
+echo 'Enter Wallet Address (NOT SEED): '
+read nimiqAddress
+echo 'Enter email address for letsencrypt: '
+read nimiqEmail
 
+#Check for possible conflicts
+toremove=""
+declare -a rmvdeps=("apache2" "nginx")
+
+for i in "${rmvdeps[@]}"
+do
+   dpkg -s "$i" &> /dev/null
+   if [ $? -eq 1 ]; then
+        echo "$i Check Complete"
+    else
+        read -p "$i detected. Do you want to remove it?(Type:YES) " rmvi
+        if [ "$rmvi" = "YES" ]; then
+                echo "Removing $i!"
+                toremove="$i $toremove"
+        fi
+fi
+done
+
+#remove possible conflicts if installed (and selected to remove)
+if [ "$toremove" != "" ]; then
+        sudo apt-get -y purge $(echo "$toremove" | xargs)
+fi
 
 #Make sure system is updated
 sudo apt-get update
@@ -40,22 +64,16 @@ sudo npm run prepare
 
 #setup Letsencrypt
 sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:certbot/certbot
+sudo add-apt-repository -y ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install -y certbot
 
-#remove apache2 if installed
-if [ "$nimiqApache" = "YES" ]; then
-    sudo apt-get remove apache2
-fi
-
-
 #Get SSL Cert
-sudo certbot certonly --standalone --preferred-challenges http -d $nimiqDomain
+sudo certbot --non-interactive --agree-tos -m $nimiqEmail certonly --standalone --preferred-challenges http -d $nimiqDomain
 
 #Generate Mining Runscript
 cd ..
 touch $nimiqScript
 chmod +x $nimiqScript
 echo "cd core/clients/nodejs/" >> $nimiqScript 
-echo "node index.js --host=$nimiqDomain --port=5566 --cert=/etc/letsencrypt/live/$nimiqDomain/cert.pem --key=/etc/letsencrypt/live/$nimiqDomain/privkey.pem -miner=$nimiqThreads --statistics=1" >> $nimiqScript
+echo "node index.js --host=$nimiqDomain --wallet-address=$nimiqAddress --port=5566 --cert=/etc/letsencrypt/live/$nimiqDomain/cert.pem --key=/etc/letsencrypt/live/$nimiqDomain/privkey.pem -miner=$nimiqThreads --statistics=1" >> $nimiqScript
